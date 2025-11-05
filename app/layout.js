034d0cc8cfb2990aa0beec4ba40340dcea555873
Lovish Singlaa <singlalovish26@gmail.com>
2025-11-05 12:47:31 +0530

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./dashboard/_components/Header";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
           <Toaster/>
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
